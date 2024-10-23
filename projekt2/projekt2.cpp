654402356db236a50143d0e83b73cdd3a255d8b0
alisterekYVL <alan.gonulka123@gmail.com>
2024-10-23 09:28:45 +0200

    Add files via upload

---
+++ projekt2/projekt2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+    float x, y;
+    std::cout << "x = ";
+    std::cin >> x;
+    std::cout << "y = ";
+    std::cin >> y;
+    float z = x > y ? x : y;
+    std::cout << "maks = " << (x > y ? x : y) << std::endl;
+}
---
+++ projekt4/projekt4.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void p1()
+{
+    float sum = 0.0;
+    int x = 0;
+    float d;
+    while (true) {
+        cout << "Podaj liczbe (0 aby zakonczyc): ";
+        cin >> d;
+        if (d == 0) {
+            break;
+        }
+        sum = sum + d;
+        x++;
+    }
+    if (x > 0) {
+        cout << "Suma = " << sum << endl;
+        cout << "Srednia = " << sum / x << endl;
+    }
+    else {
+        cout << "Nie wprowadzono zadnych liczb." << endl;
+    }
+}
+int main() {
+    p1();
+}
