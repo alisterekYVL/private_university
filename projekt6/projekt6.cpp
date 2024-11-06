14fe905233775aeedcd98b35f837c092c045de8e
alisterekYVL <alan.gonulka123@gmail.com>
2024-11-06 09:25:43 +0100

    Add files via upload

---
+++ projekt6/projekt6.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+	string s = " ala ma kota "; //string s("ala ma kota ");
+	cout << s << endl;
+
+	string s1;
+	cout << s1 << endl;
+	cout << "Wprowadz napis: ";
+	cin >> s1;
+	cout << s1;
+
+	cout << s + s1 << endl;
+
+	for (int i = 0; i < s.length(); ++i) {
+		cout << s[i];
+	}
+	cout << endl;
+
+	for (auto it = s.begin(); it != s.end(); ++it) {
+		cout << *it;
+	}
+	cout << endl;
+
+	for (const auto el : s) {
+		cout << el;
+	}
+	cout << endl;
+
+	if (s == "ala ma kota") {
+		for (auto& el : s) {
+			el = toupper(el);
+		}
+	}
+	else {
+		for (auto& el : s) {
+			el = tolower(el); 
+		}
+	}
+	cout << s << endl;
+
+} 
+
+
---
+++ projekt7/projekt7.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cout << "Podaj tekst: ";
+    getline(cin, s);
+
+    int pos = s.find('A', 0);
+    while (pos != string::npos) {
+        s[pos] = '.';
+        pos = s.find('A', pos + 1);
+    }
+
+    pos = s.find('a', 0);
+    while (pos != string::npos) {
+        s[pos] = '.';
+        pos = s.find('a', pos + 1);
+    }
+
+
+    for (int i = 0; i < s.length(); ++i) {
+        if (i % 2 == 0) {
+            s[i] = toupper(s[i]);  
+        }
+        else {
+            s[i] = tolower(s[i]); 
+        }
+    }
+
+    cout << "Przeksztalcony tekst: " << s << endl;
+
+}
