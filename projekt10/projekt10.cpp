44041dfb5e6947d170f96f4abf57dcecb2624f31
alisterekYVL <alan.gonulka123@gmail.com>
2024-11-27 09:21:01 +0100

    Add files via upload

---
+++ projekt10/projekt10.cpp
@@ -0,0 +1,118 @@
+﻿#include "projekt10.hpp"
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <list>
+#include <set>
+#include <stack>
+#include <queue>
+#include <map>
+#include <numeric>
+
+
+
+using namespace std;
+
+void show(const vector<double>& v) {
+    for (const auto el : v) {
+        cout << el << endl;
+    }
+    cout << endl;
+}
+
+void p1() {
+    vector<double> v(5, 13.3);
+    for (const auto el : v) {
+        cout << el << endl;
+    }
+
+    cout << "\n";
+
+    list<double> l;
+    l.push_back(13.6);
+    l.push_front(11.8);
+    l.insert(l.begin(), 8.7);
+    for (const auto el : l) {
+        cout << el << endl;
+    }
+
+    cout << "\n";
+
+    set<int> s;
+    s.insert(12);
+    s.insert(13);
+    s.insert(11);
+    s.insert(12);
+    for (const auto el : s) {
+        cout << el << endl;
+    }
+    cout << s.count(12) << endl;
+    cout << s.count(9) << endl;
+
+    cout << "\n";
+
+    stack<int> st;
+    st.push(11);
+    st.push(17);
+    st.push(21);
+
+    while (!st.empty()) {
+        cout << st.top() << endl;
+        st.pop();
+    }
+
+    cout << "\n";
+
+    queue<int> q;
+    q.push(12);
+    q.push(13);
+    q.push(14);
+    while (!q.empty()) {
+        cout << q.front() << endl;
+        q.pop();
+    }
+
+    cout << "\n";
+
+    map<string, int> m;
+
+    m["Polska"] = 48;
+    m["Francja"] = 148;
+
+    for (const auto& el : m) {
+        cout << el.first << " : " << el.second << endl;
+    }
+
+    cout << "\n";
+
+
+
+}
+
+void p2() {
+    vector<double> v = { 4,9,-3,21, 13};
+    show(v);
+    //sort(v.begin(), v.end());
+    //reverse(v.begin(), v.end());
+
+    sort(v.begin(), v.end(), [](const auto l, const auto r) {return l > r; });
+    show(v);
+
+    auto min = *min_element(v.begin(), v.end());
+    cout << "min = " << min << endl;
+
+    auto max = *max_element(v.begin(), v.end());
+    cout << "min = " << max << endl;
+
+    auto sum = accumulate(v.begin(), v.end(), 0.0);
+    cout << "Suma = " << sum << endl;
+
+    auto count = count_if(v.begin(), v.end(), [](const auto x) {return x > 0.0; });
+    cout << "Liczba dodatnich = " << count << endl;
+}
+
+int main() {
+
+    //p1();
+    p2();
+}
---
+++ projekt11/projekt11.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+using Vector = std::vector<double>;
+using Matrix = std::vector<std::vector<double>>;
+
+void Show(const Vector& v) {
+    for (const auto el : v) {
+        cout << el << endl;
+    }
+}
+
+void ShowMatrix(const Matrix& m) {
+    for (const auto& row : m) {
+        for (const auto& el : row) {
+            cout << el << " ";
+        }
+        cout << endl;
+    }
+}
+
+bool SaveVectorToFile(const string& fileName, const Vector& v) {
+    ofstream out(fileName);
+    if (out) {
+        for (const auto el : v) {
+            out<< el << endl;
+        }
+        out.close();
+        return true;
+    }
+    return false;
+}
+
+bool SaveMatrixToFile(const string& fileName, const Matrix& m) {
+    ofstream out2(fileName);
+    if (out2) {
+        for (const auto& row : m) {
+            for (const auto& el : row) {
+                out2 << el << "\t";
+            }
+            out2 << endl;
+        }
+        out2.close();
+        return true;
+    }
+    return false;
+}
+
+Vector LoadVectorFromFile(const string& fileName) {
+    Vector v;
+    ifstream in(fileName);
+    if (in) {
+        double x;
+        while(in >> x) {
+            v.push_back(x);
+        }
+        in.close();
+    }
+    return v;
+}
+
+Matrix LoadMatrixFromFile(const string& fileName) {
+    Matrix m;
+    ifstream in(fileName);
+    if (in) {
+        string line;
+        while (getline(in, line)) {
+            cout << line << endl;
+        }
+        in.close();
+    }
+    return m;
+}
+
+void p1() {
+    vector<double> v = { 1,4,7,-14,-1,55 };
+    Show(v);
+    SaveVectorToFile("vector.txt", v);
+}
+
+void p2() {
+    auto v = LoadVectorFromFile("vector.txt");
+    Show(v);
+}
+
+void p3() {
+    auto m = LoadMatrixFromFile("vector.txt");
+    ShowMatrix(m);
+}
+
+int main() {
+    p3();
+}
